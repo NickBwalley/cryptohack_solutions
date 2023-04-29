4d4f485dcfc022c208dab70fb5974fb97093be96
NickBwalley <nickbwalley@gmail.com>
2023-04-29 11:00:29 +0300

    lastcommit

--- encoding_solutions.py
+++ encoding_solutions.py
@@ -1,57 +1,54 @@
-from binascii import unhexlify
-from pwn import *
 import base64
 import codecs
-from Crypto.Util.number import bytes_to_long, long_to_bytes
 import json
-import sys
-import Crypto
+import telnetlib
+
+import Crypto.Util.number
+
+HOST = "socket.cryptohack.org"
+PORT = 13377
+
+tn = telnetlib.Telnet(HOST, PORT)
 
 
-r = remote('socket.cryptohack.org', 13377, level = 'debug')
+def readline():
+    return tn.read_until(b"\n")
 
-class Decoder(object):
-    def __init__(self,data):
-        self.type = ''
-        self.data = ''
-        loaded_json = json.loads(data)
-        self.type = loaded_json['type']
-        self.data = loaded_json['encoded']
 
-        print("recieved this %s,%s",self.data,self.type)
+def json_recv():
+    line = readline()
+    return json.loads(line.decode())
 
-    def calling(self):
 
-        if self.type == "base64":
-            return base64.b64decode(self.data).decode('ISO-8859-1')
+def json_send(hsh):
+    request = json.dumps(hsh).encode()
+    tn.write(request)
 
-        elif self.type == "hex":
-            return bytes.fromhex(self.data).decode('ISO-8859-1')
 
-        elif self.type == "rot13":
-            return codecs.decode(self.data, 'rot_13')
-        
-        elif self.type == "bigint":
-            len_decode = len(self.data)
-            x =  int(self.data, 16).to_bytes(len_decode, 'big')
-            return str(x, 'UTF-8').lstrip('\x00')
-
-        elif self.type == "utf-8":
-            return  ''.join(chr(o) for o in self.data)
-    
-   
-        
 for i in range(100):
-    retrieve = r.recv_raw(1024)
-    decoder = Decoder(retrieve)
-    decode = decoder.calling()
-    print("decoded ==== %s %s",i,decode)
-    decode = {
-        "decoded": decode
-    }
-    response = json.dumps(decode)
+    recieved = json_recv()
+    encoding = recieved['type']
+    encoded = recieved['encoded']
 
-    r.send(response)
+    if encoding == "base64":
+        decoded = base64.b64decode(encoded).decode()
+    elif encoding == "hex":
+        decoded = bytes.fromhex(encoded).decode()
+    elif encoding == "rot13":
+        decoded = codecs.decode(encoded, "rot13")
+    elif encoding == "bigint":
+        decoded = Crypto.Util.number.long_to_bytes(int(encoded, 16)).decode()
+    elif encoding == "utf-8":
+        decoded = "".join(chr(o) for o in encoded)
 
+    print(f"{i + 1}) {encoding}:")
+    print(f"{encoded} ---> {decoded}\n")
 
-print('\n\nthe flag is:'+r.recv(1024).decode('ISO-8859-1'))
+    json_send(
+        {"decoded": decoded}
+    )
+
+flag = readline()
+flag = json.loads(flag.decode())
+print("\nFlag:")
+print(str(flag["flag"]))
