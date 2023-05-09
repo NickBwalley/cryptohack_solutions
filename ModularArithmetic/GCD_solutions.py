23fdf17ca0e68efaf73ef272cd0f87410ef2ef51
NickBwalley <nickbwalley@gmail.com>
2023-05-09 20:37:06 +0300

    modular arithmetic

---
+++ ModularArithmetic/GCD_solutions.py
@@ -0,0 +1,18 @@
+import math
+print(math.gcd(66528, 52920))
+
+
+#-----------------BRUTEFORCE SOLUTION---------------------####################
+# import math
+
+# def gcd(a,b):
+#     print(f"a={a}, b = {b} - modulo result = {a%b}")
+#     while a%b !=0:
+#         c = a%b
+#         a = b
+#         b = c
+
+#         print(f"a = {a}, b = {b} - modulo result = {a%b}")
+
+#     return b
+# print(f"GCD: {gcd(66528,52920)}")
